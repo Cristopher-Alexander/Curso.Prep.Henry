f30ee5b11fda72ce48faabbddff1e70e8c800a12
Cristopher <cristophergarcia789@outlook.es>
2021-12-19 11:33:01 -0500

    logrado al 100%

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,8 +45,8 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var a = array.map(function(elemento,indice){
-     return cb(elemento);
+  var a = array.map(function(el,indice){
+     return cb(el);
   })
   return a;
 }
